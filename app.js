d387735fbbcbda31e434e1b14c17d85915890d80
ttaix <tansu@aibrite.com>
2017-06-08 20:07:51 +0300

    removed conn str

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ var mongoose = require('mongoose');
 var jmongo = require('jdash-mongodb').default;
 
 var connStr = 'Your connection string goes here';
-connStr = 'mongodb://localhost:27017/jdash-demo';
-
-
-
 
 var connection = mongoose.createConnection(connStr);
 
